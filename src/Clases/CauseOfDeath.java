7161118f7292ab81a16e32fa70400213fdf62ce7
timofabregat <80836792+timofabregat@users.noreply.github.com>
2021-06-03 16:35:50 -0300

    Clase MovieCastMember y actualizacion CauseOfDeath

--- src/Clases/CauseOfDeath.java
+++ src/Clases/CauseOfDeath.java
@@ -2,11 +2,17 @@ package Clases;
 
 public class CauseOfDeath {
 
+    //ATRIBUTOS
+
     private String name;
+
+    //CONSTRUCTOR
 
     public CauseOfDeath(String name) {
         this.name = name;
     }
+
+    //GETTERS
 
     public String getName() {
         return name;
---
+++ src/Clases/MovieCastMember.java
@@ -0,0 +1,41 @@
+package Clases;
+
+public class MovieCastMember {
+
+    //ATRIBUTOS
+
+    private int ordering;
+
+    private String Category;
+
+    private String job;
+
+    private ListaEnlazada<String> characters;
+
+    //CONSTRUCTOR
+
+    public MovieCastMember(int ordering, String category, String job, ListaEnlazada<String> characters) {
+        this.ordering = ordering;
+        Category = category;
+        this.job = job;
+        this.characters = characters;
+    }
+
+    //GETTERS
+
+    public int getOrdering() {
+        return ordering;
+    }
+
+    public String getCategory() {
+        return Category;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public ListaEnlazada<String> getCharacters() {
+        return characters;
+    }
+}
