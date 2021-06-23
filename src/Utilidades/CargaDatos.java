fd2829e8ea3ac0fcd24bc837f123e217012dd4dc
timofabregat <80836792+timofabregat@users.noreply.github.com>
2021-06-23 16:41:54 -0300

    optimizacion carga

--- src/Utilidades/CargaDatos.java
+++ src/Utilidades/CargaDatos.java
@@ -2,6 +2,7 @@ package Utilidades;
 
 import Entidades.*;
 import Reader.CsvIterator;
+import Reader.Reader;
 import TADs.Excepciones.UnavailableIndex;
 import TADs.Implementaciones.LinkedHashImpl;
 import TADs.Implementaciones.ListaEnlazada;
@@ -15,20 +16,37 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Iterator;
 
 
 public class CargaDatos {
 
-    public static LinkedHashImpl<String,MovieCastMember> movieCastMemberLinkedHash = new LinkedHashImpl<>(1090000);
+    public static LinkedHashImpl<String,MovieCastMember> movieCastMemberLinkedHash = new LinkedHashImpl<>(1113991);
 
-    public static MyClosedHashImpl<String,CastMember> castMemberClosedHash = new MyClosedHashImpl<>(600000);
+    public static MyClosedHashImpl<String,CastMember> castMemberClosedHash = new MyClosedHashImpl<>(396947);
 
-    public static MyClosedHashImpl<String, Movie> movieClosedHash = new MyClosedHashImpl<>(85855);
+    public static MyClosedHashImpl<String, Movie> movieClosedHash = new MyClosedHashImpl<>(114473);
 
-    public static MyClosedHashImpl<String,MovieRating> movieRatings = new MyClosedHashImpl<>(85855);
+    public static MyClosedHashImpl<String,MovieRating> movieRatings = new MyClosedHashImpl<>(114473);
 
     public static void CargaMovieCastMembers() {
-        String filename1 = "src/IMDb title_principals.csv";
+        String fileName1 = "src/IMDb title_principals.csv";
+        File principals = new File(fileName1);
+        Reader reader = new Reader(principals);
+        Iterator<String[]> readerIterator =reader.iterator();
+
+        String[] atributos = readerIterator.next();
+        if(readerIterator.hasNext()){
+            atributos = readerIterator.next();
+        }
+        while (readerIterator.hasNext()){
+            atributos = readerIterator.next();
+            MovieCastMember nuevoMovieCastMember = new MovieCastMember(atributos);
+            movieCastMemberLinkedHash.put(nuevoMovieCastMember.getImdb_name_id(),nuevoMovieCastMember);
+        }
+
+
+        /*String filename1 = "src/IMDb title_principals.csv";
         Path pathToFile1 = Paths.get(filename1);
         try (BufferedReader reader = Files.newBufferedReader(pathToFile1, StandardCharsets.UTF_8)) {
             String line = reader.readLine();
@@ -44,11 +62,27 @@ public class CargaDatos {
             }
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        }*/
     }
 
     public static void CargaCastMember() throws UnavailableIndex {
         String fileName2 = "src/IMDb names.csv";
+        File names = new File(fileName2);
+        Reader reader = new Reader(names);
+        Iterator<String[]> readerIterator =reader.iterator();
+
+        String[] atributos = readerIterator.next();
+        if(readerIterator.hasNext()){
+            atributos = readerIterator.next();
+        }
+        while (readerIterator.hasNext()){
+            atributos = readerIterator.next();
+            CastMember nuevoCastMember = new CastMember(atributos);
+            castMemberClosedHash.put(nuevoCastMember.getImdbNameId(),nuevoCastMember);
+        }
+
+
+        /*String fileName2 = "src/IMDb names.csv";
         Path pathToFile2 = Paths.get(fileName2);
         File archivo = new File(String.valueOf(pathToFile2));
         CsvIterator reader = new CsvIterator(archivo);
@@ -62,11 +96,28 @@ public class CargaDatos {
             nuevoCastMember.setCauseOfDeath(nuevaCauseOfDeath);
             castMemberClosedHash.put(nuevoCastMember.getImdbNameId(), nuevoCastMember);
             tengo = reader.hasNext();
-        }
+        }*/
     }
 
-    public static void CargaMovies() {
-        String fileName = "src/IMDb movies.csv";
+    public static void CargaMovies() throws UnavailableIndex {
+        String fileName3 = "src/IMDb movies.csv";
+        File movies = new File(fileName3);
+        Reader reader = new Reader(movies);
+        Iterator<String[]> readerIterator =reader.iterator();
+
+        String[] atributos = readerIterator.next();
+        if(readerIterator.hasNext()){
+            atributos = readerIterator.next();
+        }
+        while (readerIterator.hasNext()){
+            atributos = readerIterator.next();
+            Movie nuevaMovie = new Movie(atributos);
+            movieClosedHash.put(nuevaMovie.getImdbTitled(),nuevaMovie);
+        }
+
+
+
+        /*String fileName = "src/IMDb movies.csv";
         Path pathToFile = Paths.get(fileName);
         try(BufferedReader reader = Files.newBufferedReader(pathToFile, StandardCharsets.UTF_8)){
             String line = reader.readLine();
@@ -80,11 +131,29 @@ public class CargaDatos {
         }
         catch (IOException | UnavailableIndex e){
             e.printStackTrace();
-        }
+        }*/
     }
 
-    public static void CargaRatings(){
-        String filename3 = "src/IMDb ratings.csv";
+    public static void CargaRatings() throws UnavailableIndex {
+        String fileName4 = "src/IMDb ratings.csv";
+        File ratings = new File(fileName4);
+        Reader reader = new Reader(ratings);
+        Iterator<String[]> readerIterator =reader.iterator();
+
+        String[] atributos = readerIterator.next();
+        if(readerIterator.hasNext()){
+            atributos = readerIterator.next();
+        }
+        while (readerIterator.hasNext()){
+            atributos = readerIterator.next();
+            MovieRating nuevoMovieRating = new MovieRating(atributos);
+            movieRatings.put(nuevoMovieRating.getImbd_title_id(),nuevoMovieRating);
+
+        }
+
+
+
+        /*String filename3 = "src/IMDb ratings.csv";
         Path pathToFile3 = Paths.get(filename3);
         try(BufferedReader reader = Files.newBufferedReader(pathToFile3, StandardCharsets.UTF_8)) {
             String line = reader.readLine();
@@ -98,6 +167,6 @@ public class CargaDatos {
         }
         catch (IOException | UnavailableIndex e) {
             e.printStackTrace();
-        }
+        }*/
     }
 }
